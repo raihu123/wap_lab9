12f72f3332690e1114d7f2af26a48c9ccc891fa7
rayhanrashid <12345678asdf>
2022-04-09 10:43:47 -0500

    s

--- q2.js
+++ q2.js
@@ -1,51 +1,66 @@
-function Question(qid, answer) {
-  this.qid = qid;
-  this.answer = answer;
+class Question {
+  constructor(questionId, answer) {
+    this.questionId = questionId;
+    this.answer = answer;
+  }
+
+  checkAnswer(correctAnswer) {
+    return this.answer === correctAnswer;
+  }
 }
-Question.prototype.checkAnswer = function(answer) {
-  return this.answer === answer;
+
+class Student {
+  constructor(studentId, answers = []) {
+    this.studentId = studentId;
+    this.answers = answers;
+  }
+
+  addAnswer(question) {
+    this.answers.push(question);
+  }
 }
-function Student(studentId) {
-  this.studentId = studentId;
-  this.answers = [];
+
+class Quiz {
+  constructor(questionsArray = [], students = []) {
+    this.questions = new Map();
+    questionsArray.forEach(question => this.questions.set(question.questionId
+      , question.answer));
+    this.students = students;
+  }
+
+  scoreStudent(studentId) {
+    let student = this.students.filter(student => student.studentId === studentId)[0];
+    return student.answers.reduce((sum, currentQuestion) => {
+      let questionId = currentQuestion.questionId;
+      let correctAnswer = this.questions.get(questionId);
+      let result = currentQuestion.checkAnswer(correctAnswer);
+      if (result) {
+        sum = sum + 1;
+      }
+      return sum;
+    }, 0);
+  }
+
+  getAverageScore() {
+    return this.students.reduce((average, currentStudent, index, array) => {
+      return average + this.scoreStudent(currentStudent.studentId) / array.length;
+      }, 0
+    );
+  }
 }
-Student.prototype.addAnswer = function(question) {
-  this.answers.push(question);
-}
-function Quiz(questions, students) {
-  this.questions = new Map();
-  questions.forEach(q => this.questions.set(q.qid, q.answer));
-  this.students = students;
-}
-Quiz.prototype.scoreStudentBySid = function(studentId) {
-  const student = this.students.filter(s => s.studentId === studentId)[0];
-  return student.answers.reduce((sum, currentQuestion) => {
-    if (currentQuestion.checkAnswer(this.questions.get(currentQuestion.qid)))
-    {
-      sum = sum + 1;
-    }
-    return sum;
-  }, 0);
-}
-Quiz.prototype.getAverageScore = function() {
-  return this.students.reduce((accmulator, student, index, array) => {
-    return accmulator + this.scoreStudentBySid(student.studentId) / array.length;
-  }, 0);
-}
-const questions = [new Question(1, 'b'), new Question(2, 'a'), new Question(3, 'b')];
-const student1 = new Student(10);
-student1.addAnswer(new Question(2, 'a'));
-student1.addAnswer(new Question(3, 'b'));
-student1.addAnswer(new Question(1, 'b'));
-const student2 = new Student(11);
-student2.addAnswer(new Question(3, 'b'));
-student2.addAnswer(new Question(2, 'a'));
-student2.addAnswer(new Question(1, 'd'));
+
+const questionsArraywithCorrectAnswers = [new Question(1, 'a'),
+  new Question(2, 'b'),
+  new Question(3, 'd')];
+let student1 = new Student(1001, [new Question(1, 'b'),
+  new Question(2, 'b'),
+  new Question(3, 'b')]);
+let student2 = new Student(1002);
+student2.addAnswer(new Question(1, 'a'));
+student2.addAnswer(new Question(2, 'b'));
+student2.addAnswer(new Question(3, 'd'));
 const students = [student1, student2];
-const quiz = new Quiz(questions, students);
-let scoreforStudent10 = quiz.scoreStudentBySid(10);
-console.log(scoreforStudent10);
-let scoreforStudent11 = quiz.scoreStudentBySid(11);
-console.log(scoreforStudent11);
-let average = quiz.getAverageScore();
-console.log(average);
+let quizObj = new Quiz(questionsArraywithCorrectAnswers, students);
+console.log(quizObj.scoreStudent(1001));
+console.log(quizObj.scoreStudent(1002));
+console.log(quizObj.getAverageScore());
